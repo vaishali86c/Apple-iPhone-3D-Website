d5f3c14ff88469a9856649213254caaaa5210c59
vaishali86c <vaishali86c@gmail.com>
2024-05-27 15:02:52 +0530

    Create DisplaySection

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Quote from './sections/Quote';
 import HeroSection from './sections/HeroSection';
 import PhoneModel from './sections/PhoneModel'
 import DesignSection from './sections/DesignSection';
+import DisplaySection from './sections/DisplaySection'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <PhoneModel />
       <HeroSection />
       <DesignSection />
+      <DisplaySection />
     </>
   );
 }
---
+++ src/sections/DisplaySection.js
@@ -0,0 +1,105 @@
+import styled from "styled-components";
+
+const Section = styled.section`
+width: 100vw;
+height: 200vh;
+position: relative;
+
+display: flex;
+justify-content: space-around;
+flex-direction: column;
+
+background-color: var(--dark);
+color: var(--white);
+
+&>*:nth-child(even){
+    align-self: flex-end;
+    margin-right: 4rem;
+    text-align: right;
+}
+
+&>*:nth-child(odd){
+    margin-left: 4rem;
+}
+`
+const MainTitle = styled.h1`
+font-size: var(--fontBig);
+font-family: var(--fontL);
+background-image: linear-gradient(-45deg, var(--gradient));
+background-clip: text;
+-webkit-background-clip: text;
+-webkit-text-fill-color: transparent;
+`
+const TextBlockRight = styled.div`
+display: flex;
+flex-direction: column;
+align-items: flex-end;
+width: 50%;
+`
+const TextBlockLeft = styled.div`
+display: flex;
+flex-direction: column;
+align-items: flex-start;
+width: 50%;
+`
+
+const Title = styled.div`
+font-size: var(--fontlg);
+margin-bottom: 1rem;
+`
+const Text = styled.div`
+font-size: var(--fontxs);
+color: var(--greyLight);
+margin-bottom: 0.5rem;
+width: 55%;
+`
+const TextContainer = styled.div`
+width: 100%;
+display: flex;
+flex-direction: column;
+align-items: center
+justify-content: center;
+transform: rotate(-25deg);
+z-index: 1;
+margin-bottom: 4rem;
+
+`
+
+const MovingText = styled.h1`
+font-size: var(--fontBig);
+font-family: var(--fontL);
+background-image: linear-gradient(-45deg, var(--gradient));
+background-clip: text;
+-webkit-background-clip: text;
+-webkit-text-fill-color: transparent;
+`
+
+const DisplaySection = () => {
+    return (
+        <Section>
+            <MainTitle>
+                Immersive <br /> Display
+            </MainTitle>
+            <TextBlockRight>
+                <Title>Super Ratine XDR Display</Title>
+                <Text>
+                    Lorem ispus  , dolor  sit amet  consdbaja asdsjhdjkshdjkas elit.
+                    nsjcdjckjsad snjsndkjdjd nsdsd
+                </Text>
+            </TextBlockRight>
+            <TextBlockLeft>
+                <Title>Big is Better</Title>
+                <Text>
+                    Lorem ispus  , dolor  sit amet  consdbaja asdsjhdjkshdjkas elit.
+                    nsjcdjckjsad snjsndkjdjd nsdsd
+                </Text>
+            </TextBlockLeft>
+            <TextContainer>
+                <MovingText>Tougher then ever!</MovingText>
+                <MovingText>Every Touch Matters.</MovingText>
+            </TextContainer>
+        </Section>
+    );
+}
+
+export default DisplaySection;
