e34bea48da1c3736a11c584402bba370dcc5af4d
vaishali86c <vaishali86c@gmail.com>
2024-05-27 13:21:40 +0530

    Create DesignSection

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import { GlobalStyle } from './styles/GlobalStyle';
 import Quote from './sections/Quote';
 import HeroSection from './sections/HeroSection';
-import PhoneModel from './sections/PhoneModel';
+import PhoneModel from './sections/PhoneModel'
+import DesignSection from './sections/DesignSection';
 
 function App() {
   return (
     <>
       <GlobalStyle />
       <Quote />
-      {/* <PhoneModel /> */}
+      <PhoneModel />
       <HeroSection />
-      
+      <DesignSection />
     </>
   );
 }
